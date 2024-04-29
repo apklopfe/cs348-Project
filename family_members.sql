ed4076ab05d101c4ae0302ab9388c4929529178e
Adam Klopfenstein <112191299+apklopfe@users.noreply.github.com>
2024-04-29 02:39:07 -0400

    Update family_members.sql

--- family_members.sql
+++ family_members.sql
@@ -1,11 +1,11 @@
-INSERT INTO family_member (id, username, name, age, gender, phone, email) VALUES
-(1, 'example_username', 'John Doe', 35, 'Male', '1234567890', 'john@example.com'),
-(2, 'example_username', 'Jane Doe', 32, 'Female', '9876543210', 'jane@example.com'),
-(3, 'example_username', 'Michael Smith', 40, 'Male', '5551234567', 'michael@example.com'),
-(4, 'example_username', 'Emily Smith', 38, 'Female', '5559876543', 'emily@example.com'),
-(5, 'example_username', 'David Johnson', 28, 'Male', '1112223344', 'david@example.com'),
-(6, 'example_username', 'John Doe', 45, 'Male', '123-456-7890', 'john.doe@example.com'),
-(7, 'example_username', 'Jane Doe', 42, 'Female', '456-789-0123', 'jane.doe@example.com'),
-(8, 'example_username', 'Alice Smith', 30, 'Female', '789-012-3456', 'alice.smith@example.com'),
-(9, 'example_username', 'Bob Smith', 35, 'Male', '012-345-6789', 'bob.smith@example.com'),
-(10, 'example_username', 'Eva Johnson', 28, 'Female', '345-678-9012', 'eva.johnson@example.com');
+INSERT INTO family_member (id, name, age, gender, phone, email) VALUES
+(1, 'John Doe', 35, 'Male', '1234567890', 'john@example.com'),
+(2, 'Jane Doe', 32, 'Female', '9876543210', 'jane@example.com'),
+(3, 'Michael Smith', 40, 'Male', '5551234567', 'michael@example.com'),
+(4, 'Emily Smith', 38, 'Female', '5559876543', 'emily@example.com'),
+(5, 'David Johnson', 28, 'Male', '1112223344', 'david@example.com'),
+(6, 'John Doe', 45, 'Male', '123-456-7890', 'john.doe@example.com'),
+(7, 'Jane Doe', 42, 'Female', '456-789-0123', 'jane.doe@example.com'),
+(8, 'Alice Smith', 30, 'Female', '789-012-3456', 'alice.smith@example.com'),
+(9, 'Bob Smith', 35, 'Male', '012-345-6789', 'bob.smith@example.com'),
+(10, 'Eva Johnson', 28, 'Female', '345-678-9012', 'eva.johnson@example.com');
